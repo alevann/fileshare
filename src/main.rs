332b79650dfd55f3c8e76890d4b29fb382026a07
alevann <55453451+alevann@users.noreply.github.com>
2021-06-23 10:40:08 +0100

    added logging - cleaned up main.rs - updated gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,53 @@
-use std::process;
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
+use simplelog::*;
 use std::env;
+use std::fs::File;
 use fileshare::Config;
 
-macro_rules! e_exit {
-    () => {
-        process::exit(1)
+
+macro_rules! exit {
+    ( $($arg:tt),* ) => {
+        eprintln!($($arg,)*);
+        std::process::exit(1);
     };
 }
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args[1..]).unwrap_or_else( |e| {
-        eprintln!("{}", e);
-        e_exit!()
+    let config = Config::new(&args[1..]).unwrap_or_else(|e| {
+        exit!("{}", e);
     });
 
+    if let Err(e) = init_logging() {
+        exit!("Log initialization error: {}", e);
+    } else {
+        info!("Logging initialized successfully!")
+    }
+
     if let Err(e) = fileshare::run(config) {
-        eprintln!("Application error: {}", e);
-        e_exit!()
+        exit!("Application error: {}", e);
+    }
+}
+
+fn init_logging() -> Result<(), &'static str> {
+    let file = if let Ok(file) = File::create("./logs/log.log") {
+        file
+    } else {
+        return Err("Failed to create logfile")
+    };
+
+    if let Err(_) = WriteLogger::init(
+        LevelFilter::Trace,
+        simplelog::Config::default(),
+        file
+    ) {
+        Err("Failed to initialize logger")
+    } else {
+        Ok(())
     }
 }
